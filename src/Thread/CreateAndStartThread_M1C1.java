b5038356b58c3049d5133464c88261923c176394
cheers8oys <cheers8oys@gmail.com>
2025-04-22 15:48:15 +0900

    프로그래밍언어, 플랫폼 독립성, JDK&JRE

--- src/Thread/CreateAndStartThread_M1C1.java
+++ src/Thread/CreateAndStartThread_M1C1.java
@@ -1,5 +1,9 @@
 package Thread;
 
+// public을 붙이는 클래스는 반드시 파일명.java와 같아야 한다.
+// why?
+// 자바 컴파일러가 소스를 쉽게 찾고 관리할 수 있도록 하기 위함입니다.
+// 여러 개의 public 클래스를 한 파일에 두지 못하게 하여, 코드의 명확성과 관리성을 높여줍니다.
 class SMIFileThread extends Thread{
     @Override
     public void run() {
